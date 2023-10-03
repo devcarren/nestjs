2d9597ce4d44733b7828349f94559f90d17c137c
carren dsouza <carren.dsouza@dubaitrade.ae>
2023-10-03 21:51:12 +0400

    fixes

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 import { WinstonTypeOrmLogger } from 'src/common/winston.logger';
+
+config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: '127.0.0.1',
-  port: 5432,
-  username: 'carren',
-  password: 'Login345',
-  database: 'hr',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USRNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   // entities: [Country, Department, Employee, Job, JobHistory, Location, Region],
   synchronize: false,
@@ -15,8 +18,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   autoLoadEntities: true,
   keepConnectionAlive: true,
   extra: {
-    max: 20,
-    min: 5,
+    max: parseInt(process.env.DB_CONN_POOL_MAX),
+    min: parseInt(process.env.DB_CONN_POOL_MIN),
     idleTimeoutMillis: 30000, // 30 seconds
     connectionTimeoutMillis: 2000, // 2 seconds
   },
